b0bc2878eadd1dc1ea8a7d1057085490447c46f3
Francisco <fvitaller@itba.edu.ar>
2026-06-29 13:47:58 -0300

    cambio admin real

--- botanica-web/src/app/admin/page.js
+++ botanica-web/src/app/admin/page.js
@@ -1,7 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabase";
+// 👇 ACÁ ESTÁ EL CAMBIO: agregamos ../ extra
+import { supabase } from "../../lib/supabase"; 
 import { useRouter } from "next/navigation";
 
 export default function AdminPanel() {
